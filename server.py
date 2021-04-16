b96fd7eb2b166f32a1d713b36163702797fa90eb
kkoless <65421720+kkoless@users.noreply.github.com>
2021-04-16 13:53:32 +0400

    add list of users (online)

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-host = "127.0.0.1"
+host = socket.gethostbyname(socket.gethostname())
 port = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,6 +15,7 @@ nicknames = []
 def broadcast(message):
     for client in clients:
         client.send(message)
+        client.send(f"[USERS]{str(nicknames)}".encode("utf-8"))
 
 
 def handle(client):
@@ -25,18 +26,18 @@ def handle(client):
             broadcast(message)
         except:
             index = clients.index(client)
-            clients.remove(client)
-            client.close()
             nickname = nicknames[index]
             nicknames.remove(nickname)
+            clients.remove(client)
+            client.close()
+            broadcast(f"{nickname} left chat!\n".encode("utf-8"))
             break
 
 
 def receive():
     while True:
         client, address = server.accept()
-        print(client)
-        print(address[1])
+        print(address)
         print(f"Connected with {str(address)}")
 
         client.send("NICK".encode("utf-8"))
@@ -47,11 +48,11 @@ def receive():
 
         print(f"Nickname of the client is {nickname}")
         broadcast(f"{nickname} connected to the server!\n".encode("utf-8"))
-        client.send("Connected to the server\n".encode("utf-8"))
 
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target=handle, args=(client, ))
         thread.start()
 
 
 print("Server is running...")
 receive()
+
